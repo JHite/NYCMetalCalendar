f65bc24767d078a2133625558f4f72862c6afef7
jhite <jimi.hite@gmail.com>
2018-10-19 16:24:27 -0400

    Working on Eventwrap function

--- NYCMetalCalendar/NYCMetalCalendar.py
+++ NYCMetalCalendar/NYCMetalCalendar.py
@@ -1,4 +1,5 @@
 import requests
+import dateparser
 from bs4 import BeautifulSoup
 
 #####               Variables Begin                   ######
@@ -15,6 +16,7 @@ monthsOfYear =  ['Jan',
               'Nov',
               'Dec']
 
+icalFileLocation = "C:\Users\jhite\source\repos\NYCMetalCalendar\ical\nycmetalscene.ical"
 iCalHeading = "BEGIN:VCALENDAR \n VERSION:2.0 \n PRODID:-//DDay.iCal//NONSGML ddaysoftware.com//EN"
 
 iCalEventBegin = "BEGIN:VEVENT"
@@ -33,7 +35,28 @@ icalUid = "UID:"
 icalUrl = "URL:"
 
 icalFooter = "END:VCALENDAR"
+
+showData = {}
 ###                     Variables End                  ###
+
+#TODO:
+#create a function to convert normal date entries into zulu time
+#create a eventWrap function
+def wrapEvent(showData, File):
+    f = open(File, "w+")
+    f.write(iCalEventBegin.encode("ASCII"))
+	f.write(icalCreated + )(Convert-MilliToZulu $now) 
+	iCalDescription + $clEvent.description + $iCalDescriptionLink + $clEvent.eventUrl| Out-File -encoding ASCII -append $dest
+	$iCaldtEnd + (Convert-MilliToZulu $clEvent.endDateTime) | Out-File -encoding ASCII -append $dest
+	$iCaldtStamp + (Convert-MilliToZulu $now)| Out-File -encoding ASCII -append $dest
+	$iCaldtStart + (Convert-MilliToZulu $clEvent.startDateTime) | Out-File -encoding ASCII -append $dest
+	$iCalLocation  + $clEvent.otherLocation| Out-File -encoding ASCII -append $dest
+	$iCalSequence | Out-File -encoding ASCII -append $dest
+	$iCalSummary + $clEvent.eventName| Out-File -encoding ASCII -append $dest
+	$iCalUid + $clEvent.eventId + "@collegiatelink.net"| Out-File -encoding ASCII -append $dest
+	$iCalUrl + $clEvent.eventUrl | Out-File -encoding ASCII -append $dest
+	$iCalEventEnd | Out-File -encoding ASCII -append $dest
+}
 
 page = requests.get("http://nycmetalscene.com/")
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -45,8 +68,17 @@ for p in soup.find_all('p'):
     for month in monthsOfYear: 
         if month in p.text:
             icalCount +=1
-            print(p.text)
+            eventDate = p.text.split(':')
+            #convert p.text into a dictionary containing event details to wrap.
+            #eventDate[0] contains the day i.e Fri Oct 19th 2018 TODO: convert to zulu
+            showData["date"] = dateparser.parse(eventDate[0])
+            showData["link"] = p.a['href']
+            showData["text"] = p.a.string
+           
+            
 
 print (icalCount)
-#if p.text has a month and a link, scrape the text into ical format.
+
+
+
 
