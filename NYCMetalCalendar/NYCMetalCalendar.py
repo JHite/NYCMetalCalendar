cca431f16d99f49def96b1c32ee83ad40c414f25
jhite <jimi.hite@gmail.com>
2018-10-22 16:51:23 -0400

    .

--- NYCMetalCalendar/NYCMetalCalendar.py
+++ NYCMetalCalendar/NYCMetalCalendar.py
@@ -17,49 +17,66 @@ monthsOfYear =  ['Jan',
               'Nov',
               'Dec']
 
-icalFileLocationWin32 = "C:\Users\jhite\source\repos\NYCMetalCalendar\ical\nycmetalscene.ical"
-icalFileLocationMacOS = "\Users\jhite\Programming\ical\nycmetalscene\"
+iCalFileLocationWin32 = "C:\Users\jhite\source\repos\NYCMetalCalendar\ical\nycmetalscene.ical"
+#iCalFileLocationMacOS = "\Users\jhite\Programming\ical\nycmetalscene\"
 
 iCalHeading = "BEGIN:VCALENDAR \n VERSION:2.0 \n PRODID:-//DDay.iCal//NONSGML ddaysoftware.com//EN"
 
 iCalEventBegin = "BEGIN:VEVENT"
-iCalEventEnd ="END:VEVENT"
-icalCreated = "CREATED:"
-icalDescription = "DESCRIPTION:" 
-icalDescriptionLink = " \nAdditional Information can be found at: "
-icaldtEnd= "DTEND:" 
+iCalEventEnd = "END:VEVENT"
+iCalCreated = "CREATED:"
+iCalDescription = "DESCRIPTION:" 
+iCalDescriptionLink = " \nAdditional Information can be found at: "
+iCaldtEnd= "DTEND:" 
 
-icaldtStamp = "DTSTAMP:" 
-icaldtStart = "DTSTART:" 
-icalLocation = "LOCATION:" 
-icalSequence = "SEQUENCE:0" 
-icalSummary = "SUMMARY:" 
-icalUid = "UID:" 
-icalUrl = "URL:"
+iCaldtStamp = "DTSTAMP:" 
+iCaldtStart = "DTSTART:" 
+iCalLocation = "LOCATION:" 
+iCalSequence = "SEQUENCE:0" 
+iCalSummary = "SUMMARY:" 
+iCalUid = "UID:" 
+iCalUrl = "URL:"
 
-icalFooter = "END:VCALENDAR"
+iCalFooter = "END:VCALENDAR"
 
 showData = {}
 ###                     Variables End                  ###
 
 #TODO:
 #create a function to convert normal date entries into zulu time
-#create a eventWrap function
+#add time to Created, dtEnd, dtStamp,dtStart
+#strip location from link and add to location field
+#figure out file location differences between OSs
+
 def wrapEvent(showData, File):
     f = open(File, "w+")
     f.write(iCalEventBegin.encode("ASCII"))
-	f.write(icalCreated + )
-	iCalDescription + $clEvent.description + $iCalDescriptionLink + $clEvent.eventUrl| Out-File -encoding ASCII -append $dest
-	$iCaldtEnd + (Convert-MilliToZulu $clEvent.endDateTime) | Out-File -encoding ASCII -append $dest
-	$iCaldtStamp + (Convert-MilliToZulu $now)| Out-File -encoding ASCII -append $dest
-	$iCaldtStart + (Convert-MilliToZulu $clEvent.startDateTime) | Out-File -encoding ASCII -append $dest
-	$iCalLocation  + $clEvent.otherLocation| Out-File -encoding ASCII -append $dest
-	$iCalSequence | Out-File -encoding ASCII -append $dest
-	$iCalSummary + $clEvent.eventName| Out-File -encoding ASCII -append $dest
-	$iCalUid + $clEvent.eventId + "@collegiatelink.net"| Out-File -encoding ASCII -append $dest
-	$iCalUrl + $clEvent.eventUrl | Out-File -encoding ASCII -append $dest
-	$iCalEventEnd | Out-File -encoding ASCII -append $dest
-}
+    f.write(icalCreated.encode("ASCII"))
+    f.write((iCalDescription + showData["text"] + iCalDescriptionLink + showData["link"]).encode("ASCII"))
+    f.write(iCaldtEnd.encode("ASCII"))
+    f.write(icaldtStamp.encode("ASCII"))
+    f.write(iCaldtStart.encode("ASCII"))
+    f.write(icalLocation.encode("ASCII"))
+    f.write(iCalSequence.encode("ASCII")) 
+    f.write((iCalSummary + showData["text"]).encode("ASCII"))
+    f.write((iCalUid + showData["link"]).encode("ASCII"))
+    f.write((iCalUrl + showData["link"]).encode("ASCII"))
+    f.write(iCalEventEnd.encode("ASCII"))
+
+def wrapEventConsole(showData):
+    print(iCalEventBegin.encode("ASCII"))
+    print(iCalCreated.encode("ASCII"))
+    print((iCalDescription + showData["text"] + iCalDescriptionLink + showData["link"]).encode("ASCII"))
+    print(iCaldtEnd.encode("ASCII"))
+    print(iCaldtStamp.encode("ASCII"))
+    print(iCaldtStart.encode("ASCII"))
+    print(iCalLocation.encode("ASCII"))
+    print(iCalSequence.encode("ASCII")) 
+    print((iCalSummary + showData["text"]).encode("ASCII"))
+    print((iCalUid + showData["link"]).encode("ASCII"))
+    print((iCalUrl + showData["link"]).encode("ASCII"))
+    print(iCalEventEnd.encode("ASCII"))
+
 
 page = requests.get("http://nycmetalscene.com/")
 soup = BeautifulSoup(page.text, 'html.parser')
