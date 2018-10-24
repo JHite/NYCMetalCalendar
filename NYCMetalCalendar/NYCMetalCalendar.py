edc689b11ffb598ba4418b277f26f9147a4a1baf
jhite <jimi.hite@gmail.com>
2018-10-24 15:05:43 -0400

    iCal formatting now functioning in Console

--- NYCMetalCalendar/NYCMetalCalendar.py
+++ NYCMetalCalendar/NYCMetalCalendar.py
@@ -1,6 +1,7 @@
 import requests
 import dateparser
 import os
+from unidecode import unidecode
 from bs4 import BeautifulSoup
 
 #####               Variables Begin                   ######
@@ -17,7 +18,7 @@ monthsOfYear =  ['Jan',
               'Nov',
               'Dec']
 
-iCalFileLocationWin32 = "C:\Users\jhite\source\repos\NYCMetalCalendar\ical\nycmetalscene.ical"
+#iCalFileLocationWin32 = "C:\Users\jhite\source\repos\NYCMetalCalendar\ical\nycmetalscene.ical"
 #iCalFileLocationMacOS = "\Users\jhite\Programming\ical\nycmetalscene\"
 
 iCalHeading = "BEGIN:VCALENDAR \n VERSION:2.0 \n PRODID:-//DDay.iCal//NONSGML ddaysoftware.com//EN"
@@ -40,6 +41,7 @@ iCalUrl = "URL:"
 iCalFooter = "END:VCALENDAR"
 
 showData = {}
+todaysDateData = {"date" : dateparser.parse('today')}
 ###                     Variables End                  ###
 
 #TODO:
@@ -51,7 +53,7 @@ showData = {}
 def wrapEvent(showData, File):
     f = open(File, "w+")
     f.write(iCalEventBegin.encode("ASCII"))
-    f.write(icalCreated.encode("ASCII"))
+    f.write((icalCreated + printShowDataDateZulu(todaysDateData)).encode("ASCII"))
     f.write((iCalDescription + showData["text"] + iCalDescriptionLink + showData["link"]).encode("ASCII"))
     f.write(iCaldtEnd.encode("ASCII"))
     f.write(icaldtStamp.encode("ASCII"))
@@ -65,40 +67,76 @@ def wrapEvent(showData, File):
 
 def wrapEventConsole(showData):
     print(iCalEventBegin.encode("ASCII"))
-    print(iCalCreated.encode("ASCII"))
+    print((iCalCreated + printShowDataDateZulu(todaysDateData)).encode("ASCII"))
     print((iCalDescription + showData["text"] + iCalDescriptionLink + showData["link"]).encode("ASCII"))
-    print(iCaldtEnd.encode("ASCII"))
-    print(iCaldtStamp.encode("ASCII"))
-    print(iCaldtStart.encode("ASCII"))
-    print(iCalLocation.encode("ASCII"))
+    print((iCaldtEnd + printShowDataDateZulu(showData)).encode("ASCII"))
+    print((iCaldtStamp + printShowDataDateZulu(showData)).encode("ASCII"))
+    print((iCaldtStart + printShowDataDateZulu(showData)).encode("ASCII"))
+    print((iCalLocation + showData["loc"]).encode("ASCII"))
     print(iCalSequence.encode("ASCII")) 
     print((iCalSummary + showData["text"]).encode("ASCII"))
     print((iCalUid + showData["link"]).encode("ASCII"))
     print((iCalUrl + showData["link"]).encode("ASCII"))
     print(iCalEventEnd.encode("ASCII"))
 
+def printShowDataDateZulu(date):
+    if date["date"].month  < 10:
+        return str(date["date"].year) + "0" + str(date["date"].month) + str(date["date"].day) + "T000000Z"
+    return str(date["date"].year) + str(date["date"].month) + str(date["date"].day) + "T000000Z"
 
-page = requests.get("http://nycmetalscene.com/")
-soup = BeautifulSoup(page.text, 'html.parser')
+def main():
+    page = requests.get("http://nycmetalscene.com/")
+    soup = BeautifulSoup(page.text, 'html.parser')
 
 
-icalCount  = 0
+    icalCount  = 0
 
-for p in soup.find_all('p'):
-    for month in monthsOfYear: 
-        if month in p.text:
-            icalCount +=1
-            eventDate = p.text.split(':')
-            #convert p.text into a dictionary containing event details to wrap.
-            #eventDate[0] contains the day i.e Fri Oct 19th 2018 TODO: convert to zulu
-            showData["date"] = dateparser.parse(eventDate[0])
-            showData["link"] = p.a['href']
-            showData["text"] = p.a.string
+    for p in soup.find_all('p'):
+        for month in monthsOfYear: 
+            if month in p.text:
+                icalCount +=1
+                eventDate = p.text.split(':')
+                eventLoc = p.text.split(" at ")
+                #convert p.text into a dictionary containing event details to wrap.
+                #eventDate[0] contains the day i.e Fri Oct 19th 2018 TODO: convert to zulu
+                showData["date"] = dateparser.parse(eventDate[0])
+                showData["link"] = p.a['href']
+                showData["text"] = p.a.string
+                showData["loc"] = eventLoc[len(eventLoc) -1]
+                if len(eventLoc[len(eventLoc) -1]) > 50:
+                    showData["loc"] = "Check event details"
+            
+                wrapEventConsole(showData)
            
             
 
-print (icalCount)
-
-
+    #print (icalCount)
+def test():
+    page = requests.get("http://nycmetalscene.com/")
+    newPage = page.text.replace("Thurs.","Thu.")
+    newSoup = BeautifulSoup(newPage, "html.parser")
+    for p in newSoup.find_all('p'):
+        for month in monthsOfYear: 
+            eventDate = p.text.split(':')
+            eventLoc = p.text.split(" at ")
+            if month in p.text:
+                if dateparser.parse(eventDate[0]):
+                
+                    #convert p.text into a dictionary containing event details to wrap.
+                    #eventDate[0] contains the day i.e Fri Oct 19th 2018 TODO: convert to zulu
+                    showData["date"] = dateparser.parse(eventDate[0])
+                    showData["link"] = " "
+                    #showData["text"] = unidecode(p.a.string)
+                    showData["text"] = unidecode(p.text)
+                    showData["loc"] = unidecode(eventLoc[len(eventLoc) -1])
+                    if len(eventLoc[len(eventLoc) -1]) > 50:
+                        showData["loc"] = "Check event details"
+                    if p.a:
+                        if p.a['href']:
+                            showData["link"] = p.a['href']
+                        
+            
+                    wrapEventConsole(showData)
+test()
 
 
